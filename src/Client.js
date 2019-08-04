8df1b33cd35fb8fa687fc35d20d30a6580173c44
aliyss_snow <mangoworksbeta@gmail.com>
2019-08-04 11:53:13 +0200

    Everything Works: spacing fixed.

--- src/Client.js
+++ src/Client.js
@@ -14,183 +14,183 @@ const Message = require('./structures/Message');
  * @extends {EventEmitter}
  */
 class Client extends EventEmitter {
-	constructor(options = {}) {
-		super();
+    constructor(options = {}) {
+        super();
 
-		this.options = Util.mergeDefault(DefaultOptions, options);
+        this.options = Util.mergeDefault(DefaultOptions, options);
 
-		this.pupBrowser = null;
-		this.pupPage = null;
-	}
+        this.pupBrowser = null;
+        this.pupPage = null;
+    }
 
-	/**
-	 * Sets up events and requirements, kicks off authentication request
-	 */
-	async initialize() {
-		const browser = await puppeteer.launch(this.options.puppeteer);
-		const page = await browser.newPage();
-		page.setUserAgent(UserAgent);
+    /**
+     * Sets up events and requirements, kicks off authentication request
+     */
+    async initialize() {
+        const browser = await puppeteer.launch(this.options.puppeteer);
+        const page = await browser.newPage();
+        page.setUserAgent(UserAgent);
 
-		if(this.options.session) {
-			await page.evaluateOnNewDocument (
-				session => {
-					localStorage.clear();
-					localStorage.setItem("WABrowserId", session.WABrowserId);
-					localStorage.setItem("WASecretBundle", session.WASecretBundle);
-					localStorage.setItem("WAToken1", session.WAToken1);
-					localStorage.setItem("WAToken2", session.WAToken2);
-				}, this.options.session);
-		}
+        if(this.options.session) {
+            await page.evaluateOnNewDocument (
+                session => {
+                    localStorage.clear();
+                    localStorage.setItem("WABrowserId", session.WABrowserId);
+                    localStorage.setItem("WASecretBundle", session.WASecretBundle);
+                    localStorage.setItem("WAToken1", session.WAToken1);
+                    localStorage.setItem("WAToken2", session.WAToken2);
+                }, this.options.session);
+        }
 
-		await page.goto(WhatsWebURL);
+        await page.goto(WhatsWebURL);
 
-		const KEEP_PHONE_CONNECTED_IMG_SELECTOR = '._1wSzK';
+        const KEEP_PHONE_CONNECTED_IMG_SELECTOR = '._1wSzK';
 
-		if(this.options.session) {
-			// Check if session restore was successfull
-			try {
-				await page.waitForSelector(KEEP_PHONE_CONNECTED_IMG_SELECTOR, {timeout: 5000});
-			} catch(err) {
-				if(err.name === 'TimeoutError') {
-					this.emit(Events.AUTHENTICATION_FAILURE, 'Unable to log in. Are the session details valid?');
-					browser.close();
+        if(this.options.session) {
+            // Check if session restore was successfull
+            try {
+                await page.waitForSelector(KEEP_PHONE_CONNECTED_IMG_SELECTOR, {timeout: 5000});
+            } catch(err) {
+                if(err.name === 'TimeoutError') {
+                    this.emit(Events.AUTHENTICATION_FAILURE, 'Unable to log in. Are the session details valid?');
+                    browser.close();
 
-					return;
-				}
+                    return;
+                }
 
-				throw err;
-			}
+                throw err;
+            }
 
-		} else {
-			// Wait for QR Code
+        } else {
+            // Wait for QR Code
 
-			const QR_CONTAINER_SELECTOR = '._2d3Jz';
-			const QR_VALUE_SELECTOR = '._1pw2F';
+            const QR_CONTAINER_SELECTOR = '._2d3Jz';
+            const QR_VALUE_SELECTOR = '._1pw2F';
 
-			await page.waitForSelector(QR_CONTAINER_SELECTOR);
+            await page.waitForSelector(QR_CONTAINER_SELECTOR);
 
-			const qr = await page.$eval(QR_VALUE_SELECTOR, node => node.getAttribute('data-ref'));
-			this.emit(Events.QR_RECEIVED, qr);
+            const qr = await page.$eval(QR_VALUE_SELECTOR, node => node.getAttribute('data-ref'));
+            this.emit(Events.QR_RECEIVED, qr);
 
-			// Wait for code scan
-			await page.waitForSelector(KEEP_PHONE_CONNECTED_IMG_SELECTOR, {timeout: 0});
-		}
+            // Wait for code scan
+            await page.waitForSelector(KEEP_PHONE_CONNECTED_IMG_SELECTOR, {timeout: 0});
+        }
 
-		await page.evaluate(ExposeStore);
+        await page.evaluate(ExposeStore);
 
-		// Get session tokens
-		const localStorage = JSON.parse(await page.evaluate(() => {
-			return JSON.stringify(window.localStorage);
-		}));
+        // Get session tokens
+        const localStorage = JSON.parse(await page.evaluate(() => {
+            return JSON.stringify(window.localStorage);
+        }));
 
-		const session = {
-			WABrowserId: localStorage.WABrowserId,
-			WASecretBundle: localStorage.WASecretBundle,
-			WAToken1: localStorage.WAToken1,
-			WAToken2: localStorage.WAToken2
-		}
+        const session = {
+            WABrowserId: localStorage.WABrowserId,
+            WASecretBundle: localStorage.WASecretBundle,
+            WAToken1: localStorage.WAToken1,
+            WAToken2: localStorage.WAToken2
+        }
 
-		this.emit(Events.AUTHENTICATED, session);
+        this.emit(Events.AUTHENTICATED, session);
 
-		// Check Store Injection
-		await page.waitForFunction('window.Store != undefined');
+        // Check Store Injection
+        await page.waitForFunction('window.Store != undefined');
 
-		//Load extra serialized props
-		const models = [Message];
-		for (let model of models) {
-			await page.evaluate(LoadExtraProps, model.WAppModel, model.extraFields);
-		}
+        //Load extra serialized props
+        const models = [Message];
+        for (let model of models) {
+            await page.evaluate(LoadExtraProps, model.WAppModel, model.extraFields);
+        }
 
-		await page.evaluate(LoadCustomSerializers);
+        await page.evaluate(LoadCustomSerializers);
 
-		// Register events
-		await page.exposeFunction('onAddMessageEvent', msg => {
+        // Register events
+        await page.exposeFunction('onAddMessageEvent', msg => {
 
-			if (!msg.isNewMsg) return;
-			this.emit(Events.MESSAGE_CREATE, new Message(this, msg));
-		});
+            if (!msg.isNewMsg) return;
+            this.emit(Events.MESSAGE_CREATE, new Message(this, msg));
+        });
 
-		await page.exposeFunction('onConnectionChangedEvent', (conn, connected) => {
-			if (!connected) {
-				this.emit(Events.DISCONNECTED);
-				this.destroy();
-			}
-		})
+        await page.exposeFunction('onConnectionChangedEvent', (conn, connected) => {
+            if (!connected) {
+                this.emit(Events.DISCONNECTED);
+                this.destroy();
+            }
+        })
 
-		await page.evaluate(() => {
-			Store.Msg.on('add', onAddMessageEvent);
-			Store.Conn.on('change:connected', onConnectionChangedEvent);
-		})
+        await page.evaluate(() => {
+            Store.Msg.on('add', onAddMessageEvent);
+            Store.Conn.on('change:connected', onConnectionChangedEvent);
+        })
 
-		this.pupBrowser = browser;
-		this.pupPage = page;
+        this.pupBrowser = browser;
+        this.pupPage = page;
 
-		this.emit(Events.READY);
-	}
+        this.emit(Events.READY);
+    }
 
-	async destroy() {
-		await this.pupBrowser.close();
-	}
+    async destroy() {
+        await this.pupBrowser.close();
+    }
 
-	/**
-	 * Send a message to a specific chatId
-	 * @param {string} chatId
-	 * @param {string} message
-	 */
-	async sendMessage(chatId, message) {
-		let msg = {};
-		if (message && typeof message === 'object' && message.constructor === Object) {
-			msg = message;
-		} else {
-			msg = {
-				linkPreview: null,
-				mentions: [],
-				quotedMsg: null,
-				quotedMsgAdminGroupJid: null,
-				body: message
-			}
-		}
-		let last_message = await this.pupPage.evaluate(async (chatId, msg) => {
-			msg.mentionedJidList = [];
-			for (let i = 0; i < msg.mentions.length; i++) {
-				let user = await Store.Contact.serialize().find(x => x.id.user === msg.mentions[i].split("@")[0]);
-				if (user && msg.body.includes("@" + msg.mentions[i].split("@")[0])) {
-					msg.mentionedJidList.push(user.id);
-				}
-			}
-			await Store.SendMessage(Store.Chat.get(chatId), msg.body, {linkPreview : msg.linkPreview, mentionedJidList : msg.mentionedJidList, quotedMsg : msg.quotedMsg, quotedMsgAdminGroupJid : msg.quotedMsgAdminGroupJid});
-			return Store.Chat.get(chatId).msgs._last.serialize()
-		}, chatId, msg);
-		last_message = new Message(this, last_message);
-		if (last_message.body === msg.body) {
-			return last_message
-		}
+    /**
+     * Send a message to a specific chatId
+     * @param {string} chatId
+     * @param {string} message
+     */
+    async sendMessage(chatId, message) {
+        let msg = {};
+        if (message && typeof message === 'object' && message.constructor === Object) {
+            msg = message;
+        } else {
+            msg = {
+                linkPreview: null,
+                mentions: [],
+                quotedMsg: null,
+                quotedMsgAdminGroupJid: null,
+                body: message
+            }
+        }
+        let last_message = await this.pupPage.evaluate(async (chatId, msg) => {
+            msg.mentionedJidList = [];
+            for (let i = 0; i < msg.mentions.length; i++) {
+                let user = await Store.Contact.serialize().find(x => x.id.user === msg.mentions[i].split("@")[0]);
+                if (user && msg.body.includes("@" + msg.mentions[i].split("@")[0])) {
+                    msg.mentionedJidList.push(user.id);
+                }
+            }
+            await Store.SendMessage(Store.Chat.get(chatId), msg.body, {linkPreview : msg.linkPreview, mentionedJidList : msg.mentionedJidList, quotedMsg : msg.quotedMsg, quotedMsgAdminGroupJid : msg.quotedMsgAdminGroupJid});
+            return Store.Chat.get(chatId).msgs._last.serialize()
+        }, chatId, msg);
+        last_message = new Message(this, last_message);
+        if (last_message.body === msg.body) {
+            return last_message
+        }
 
-	}
+    }
 
-	/**
-	 * Get all current chat instances
-	 */
-	async getChats() {
-		// let chats = await this.pupPage.evaluate(() => {
-		//     return Store.Chat.serialize()
-		// });
+    /**
+     * Get all current chat instances
+     */
+    async getChats() {
+        // let chats = await this.pupPage.evaluate(() => {
+        //     return Store.Chat.serialize()
+        // });
 
-		// return chats.map(chatData => ChatFactory.create(this, chatData));
-		throw new Error('NOT IMPLEMENTED')
-	}
+        // return chats.map(chatData => ChatFactory.create(this, chatData));
+        throw new Error('NOT IMPLEMENTED')
+    }
 
-	/**
-	 * Get chat instance by ID
-	 * @param {string} chatId
-	 */
-	async getChatById(chatId) {
-		let chat = await this.pupPage.evaluate(chatId => {
-			return WWebJS.getChat(chatId);
-		}, chatId);
+    /**
+     * Get chat instance by ID
+     * @param {string} chatId
+     */
+    async getChatById(chatId) {
+        let chat = await this.pupPage.evaluate(chatId => {
+            return WWebJS.getChat(chatId);
+        }, chatId);
 
-		return ChatFactory.create(this, chat);
-	}
+        return ChatFactory.create(this, chat);
+    }
 
 }
 
