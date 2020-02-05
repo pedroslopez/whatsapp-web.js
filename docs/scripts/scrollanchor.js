62e264231d2ab086351c7317604ca39d3ad33d15
Pedro Lopez <pedroslopez@me.com>
2020-02-05 01:07:56 -0400

    docs: add generated docs

---
+++ docs/scripts/scrollanchor.js
@@ -0,0 +1,1 @@
+!function(){function o(n){var o,t,e=document.getElementById(n.replace(/^#/,""));e&&(t=e.getBoundingClientRect(),o=t.top+window.pageYOffset,setTimeout(function(){window.scrollTo(0,o-50)},5))}window.addEventListener("load",function(){var n=window.location.hash;n&&"#"!==n&&o(n),window.addEventListener("hashchange",function(){o(window.location.hash)})})}();
