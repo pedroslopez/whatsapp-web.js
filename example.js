065a9212bab0b315e8d32959c8c660f04cdcabec
Pedro Lopez <pedroslopez@me.com>
2020-02-08 00:54:21 -0400

    add another location example

--- example.js
+++ example.js
@@ -123,6 +123,8 @@ client.on('message', async msg => {
             client.sendMessage(msg.from, attachmentData, {caption: 'Here\'s your requested media.'});
         }
         
+    } else if(msg.body == '!location') {
+        msg.reply(new Location(37.422, -122.084, 'Googleplex\nGoogle Headquarters'));
     } else if(msg.location) {
         msg.reply(msg.location);
     }
