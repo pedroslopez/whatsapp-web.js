044f62216bbc947e992c04ae59a83586e6f77e5e
Pedro Lopez <pedroslopez@me.com>
2020-04-11 19:45:06 -0400

    feat: link previews
    After this commit, previews for links will be enabled by default. To disable it, use linkPreview: false as an option when sending the message.
    
    close #98

--- src/Client.js
+++ src/Client.js
@@ -381,6 +381,7 @@ class Client extends EventEmitter {
      */
     async sendMessage(chatId, content, options = {}) {
         let internalOptions = {
+            linkPreview: options.linkPreview === false ? undefined : true,
             caption: options.caption,
             quotedMessageId: options.quotedMessageId,
             mentionedJidList: Array.isArray(options.mentions) ? options.mentions.map(contact => contact.id._serialized) : []
