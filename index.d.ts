50b121a02522079343033c7817d4430fcec621cf
Lucas Rosa <contato@lucasrosa.com.br>
2020-11-07 21:00:45 -0300

    feat: Get the registered chat id in whatsapp

--- index.d.ts
+++ index.d.ts
@@ -65,6 +65,9 @@ declare namespace WAWebJS {
         /** Check if a given ID is registered in whatsapp */
         isRegisteredUser(contactId: string): Promise<boolean>
 
+        /** Get the registered chat id in whatsapp */
+        getRegisteredChatId(chatId: string): Promise<string>
+
         /**
          * Mutes the Chat until a specified date
          * @param chatId ID of the chat that will be muted
