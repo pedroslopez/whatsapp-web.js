6b96ef5fb17e64b54851dd15886fdc9349ffabdd
Pedro Lopez <pedroslopez@me.com>
2020-12-21 22:49:20 -0400

    docs: fix slight typo

--- src/Client.js
+++ src/Client.js
@@ -420,7 +420,7 @@ class Client extends EventEmitter {
      * @typedef {Object} MessageSendOptions
      * @property {boolean} [linkPreview=true] - Show links preview
      * @property {boolean} [sendAudioAsVoice=false] - Send audio as voice message
-     * @property {boolean} [sendMediaAsDocument=false] = Send media as a document
+     * @property {boolean} [sendMediaAsDocument=false] - Send media as a document
      * @property {boolean} [parseVCards=true] - Automatically parse vCards and send them as contacts
      * @property {string} [caption] - Image or video caption
      * @property {string} [quotedMessageId] - Id of the message that is being quoted (or replied to)
