2558af1c500cbcc1f80a7d80a2e8084cbaefadd5
PurpShell <52720753+PurpShell@users.noreply.github.com>
2021-02-12 14:40:10 +0200

    Update Util.js

--- src/util/Util.js
+++ src/util/Util.js
@@ -184,7 +184,7 @@ class Util {
                 const stickerPackId = hash;
                 const packname = metadata.name || 'undefined';
                 const author = metadata.author || 'undefined';
-                const json = { 'sticker-pack-id': stickerpackid, 'sticker-pack-name': packname, 'sticker-pack-publisher': author };
+                const json = { 'sticker-pack-id': stickerPackId, 'sticker-pack-name': packname, 'sticker-pack-publisher': author };
                 let exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
                 let jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8');
                 let exif = Buffer.concat([exifAttr, jsonBuffer]);
