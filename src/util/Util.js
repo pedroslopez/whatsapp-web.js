49c8164ed56052bf1945ea7a997e4789ef547c09
Rajeh Taher <52720753+PurpShell@users.noreply.github.com>
2021-04-18 15:15:22 +0300

    :rotating_light: removing try/catch

--- src/util/Util.js
+++ src/util/Util.js
@@ -177,19 +177,17 @@ class Util {
         if (metadata.name || metadata.author) {
             const img = new webp.Image();
             const hash = this.generateHash(32);
-            try {
-                const stickerPackId = hash;
-                const packname = metadata.name || 'undefined';
-                const author = metadata.author || 'undefined';
-                const json = { 'sticker-pack-id': stickerPackId, 'sticker-pack-name': packname, 'sticker-pack-publisher': author };
-                let exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
-                let jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8');
-                let exif = Buffer.concat([exifAttr, jsonBuffer]);
-                exif.writeUIntLE(jsonBuffer.length, 14, 4);
-                await img.loadBuffer((webpMedia.data).toBuffer());
-                img.exif = exif;
-                webpMedia.data = (await img.saveBuffer()).toString("base64");
-            } 
+            const stickerPackId = hash;
+            const packname = metadata.name || 'undefined';
+            const author = metadata.author || 'undefined';
+            const json = { 'sticker-pack-id': stickerPackId, 'sticker-pack-name': packname, 'sticker-pack-publisher': author };
+            let exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
+            let jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8');
+            let exif = Buffer.concat([exifAttr, jsonBuffer]);
+            exif.writeUIntLE(jsonBuffer.length, 14, 4);
+            await img.loadBuffer((webpMedia.data).toBuffer());
+            img.exif = exif;
+            webpMedia.data = (await img.saveBuffer()).toString("base64");
         }
 
         return webpMedia;
