639503196ac90f7e5bba434536b00c4861798e57
Rajeh Taher <52720753+PurpShell@users.noreply.github.com>
2021-05-03 20:34:10 +0300

    :label: types

--- src/structures/MessageMedia.js
+++ src/structures/MessageMedia.js
@@ -8,7 +8,8 @@ const mime = require('mime');
  * Media attached to a message
  * @param {string} mimetype MIME type of the attachment
  * @param {string} data Base64-encoded data of the file
- * @param {?string} filename Document file name
+ * @param {?string} filename Document file name. Value can be null
+ * @param {?number} filesize Document file size in bytes. Value can be null
  */
 class MessageMedia {
     constructor(mimetype, data, filename, filesize) {
@@ -31,7 +32,7 @@ class MessageMedia {
         this.filename = filename;
 
         /**
-         * Size of the file in bytes
+         * Document file size in bytes. Value can be null
          * @type {?number}
          */
         this.filesize = filesize;
