da610c200b8fd9d51e805833f86f4b3a401602b8
Pedro S. Lopez <pslamoros@hotmail.com>
2021-05-31 19:12:40 -0400

    fix: don't set name/author if not defined

--- src/util/Util.js
+++ src/util/Util.js
@@ -171,14 +171,13 @@ class Util {
         else 
             throw new Error('Invalid media format');
 
-
         if (metadata.name || metadata.author) {
             const img = new webp.Image();
             const hash = this.generateHash(32);
             const stickerPackId = hash;
-            const packname = metadata.name || 'undefined';
-            const author = metadata.author || 'undefined';
-            const categories = metadata.categories || [""];
+            const packname = metadata.name;
+            const author = metadata.author;
+            const categories = metadata.categories || [''];
             const json = { 'sticker-pack-id': stickerPackId, 'sticker-pack-name': packname, 'sticker-pack-publisher': author, 'emojis': categories };
             let exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00]);
             let jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8');
