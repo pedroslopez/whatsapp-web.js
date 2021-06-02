644fa9937177336722d9e313da14089fb2fad7fc
JoseHM8A <70950705+JoseHM8A@users.noreply.github.com>
2021-06-02 08:24:11 -0300

    Update index.d.ts
    Added a description to the inviteV4 object
    
    Renamed "invite" by "inviteV4"

--- index.d.ts
+++ index.d.ts
@@ -508,8 +508,15 @@ declare namespace WAWebJS {
         location: Location,
         /** List of vCards contained in the message */
         vCards: string[],
-        /** Invite v4 info */
-        invite: object
+        /**
+         * inviteCode
+         * inviteExp - Code expiration time
+         * inviteGrp - Invite group Jid
+         * inviteGrpName - Group name
+         * inviteFrom - User who sent the invitation
+         * inviteTo - User who received the invitation
+         */
+        inviteV4: object
         /** MediaKey that represents the sticker 'ID' */
         mediaKey?: string,
         /** Indicates the mentions in the message body. */
