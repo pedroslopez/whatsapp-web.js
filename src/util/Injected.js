26f56c7386c8c52c1193506ef0221dee6f5b8674
tuyuribr <45042245+tuyuribr@users.noreply.github.com>
2021-06-29 18:26:31 -0300

    Get Call module

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -35,6 +35,7 @@ exports.ExposeStore = (moduleRaidStr) => {
     window.Store.Features = window.mR.findModule('FEATURE_CHANGE_EVENT')[0].default;
     window.Store.QueryOrder = window.mR.findModule('queryOrder')[0];
     window.Store.QueryProduct = window.mR.findModule('queryProduct')[0];
+    window.Store.Call = window.mR.findModule('CallCollection')[0].default;
 };
 
 exports.LoadUtils = () => {
