3461cbb6d0bad1abfab24f168e755402a5708a87
tuyuribr <45042245+tuyuribr@users.noreply.github.com>
2021-07-14 13:55:59 -0300

    buttonFormat

--- src/structures/Buttons.js
+++ src/structures/Buttons.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const MessageMedia = require('./MessageMedia');
-
+const Util = require('../util/Util');
 /**
  * Message type buttons
  */
@@ -31,20 +31,36 @@ class Buttons {
          */
         this.footer = footer;
 
-        /**
-         * buttons of message
-         * @type {Array<Array<string>>}
-         */
-        this.buttons = buttons;
-        
         if (body instanceof MessageMedia) {
             this.type = 'image';
             this.title = '';
         }else{
             this.type = 'chat';
         }
-        
+
+        /**
+         * buttons of message
+         * @type {Array<Array<string>>}
+         */
+        this.buttons = this._format(buttons);
+        if(!this.buttons.length){ throw "[BT01] No buttons";}
+                
     }
+
+    /**
+     * Creates button array from simple array
+     * @param {Array<Array<string>>} buttons
+     * @returns {Array<Array<string>>}
+     */
+    _format(buttons){
+        buttons = buttons.slice(0,3); // phone users can only see 3 buttons, so lets limit this
+        let out = [];
+        for (let btn of buttons) { //currently only type '1' is supported so lets just create the new array
+            out.push({"buttonId":Util.generateHash(6),"buttonText":{"displayText":btn.body},"type":1})
+        }
+        return out;
+    }
+    
 }
 
 module.exports = Buttons;
