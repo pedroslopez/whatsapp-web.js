c80ed5813eea237a4cd577dea0559faf9efd1389
Rajeh Taher <rajeh@reforward.dev>
2021-08-10 22:44:56 +0300

    fix for jsdog

--- src/structures/MessageMedia.js
+++ src/structures/MessageMedia.js
@@ -5,7 +5,7 @@ const path = require('path');
 const mime = require('mime');
 const fetch = require('node-fetch');
 const { URL } = require('url');
-const Client = require('../index.js');  
+const { Client } = require('whatsapp-web.js');  
 /**
  * Media attached to a message
  * @param {string} mimetype MIME type of the attachment
