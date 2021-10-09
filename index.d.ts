375623a9daa64707a87822a9753a021ff1c076d3
Rajeh Taher <rajeh@reforward.dev>
2021-10-09 04:17:15 +0300

    Updated type so no TS error when sending list/buttons

--- index.d.ts
+++ index.d.ts
@@ -678,7 +678,7 @@ declare namespace WAWebJS {
         static fromUrl: (url: string, options?: MediaFromURLOptions) => Promise<MessageMedia>
     }
 
-    export type MessageContent = string | MessageMedia | Location | Contact | Contact[]
+    export type MessageContent = string | MessageMedia | Location | Contact | Contact[] | List | Buttons 
 
     /**
      * Represents a Contact on WhatsApp
