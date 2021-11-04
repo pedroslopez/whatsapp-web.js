7d08907117f9a9777382443e7754b451838c6822
Rajeh Taher <rajeh@reforward.dev>
2021-11-04 23:48:35 +0200

    Update Client.js

--- src/Client.js
+++ src/Client.js
@@ -618,8 +618,8 @@ class Client extends EventEmitter {
         if(!inviteInfo.inviteCode) throw 'Invalid invite code, try passing the message.inviteV4 object';
         if (inviteInfo.inviteCodeExp == 0) throw 'Expired invite code';
         return await this.pupPage.evaluate(async inviteInfo => {
-            let { groupId, fromId, inviteCode, inviteCodeExp, toId } = inviteInfo;
-            return await window.Store.Wap.acceptGroupV4Invite(groupId, fromId, inviteCode, String(inviteCodeExp), toId);
+             let { groupId, fromId, inviteCode, inviteCodeExp, toId } = inviteInfo;
+             return await window.Store.Wap.acceptGroupV4Invite(groupId, fromId, inviteCode, String(inviteCodeExp), toId);
         }, inviteInfo);
     }
     
