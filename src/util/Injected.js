421588d64ca98a1a5c6e791156bec8e83ec81b0f
tuyuribr <45042245+tuyuribr@users.noreply.github.com>
2021-12-07 22:58:05 -0300

    queryLinkPreview

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -12,6 +12,8 @@ exports.ExposeStore = (moduleRaidStr) => {
     window.Store.Wap = window.mR.findModule('wap')[0].default;
     window.Store.ProfilePic = window.mR.findModule('profilePicFind')[0];
     window.Store.Status = window.mR.findModule('sendPresenceAvailable')[0];
+    window.Store.Query = window.mR.findModule('queryLinkPreview')[0].default; //we have a lot of new functions here
+    
     window.Store.SendSeen = window.mR.findModule('sendSeen')[0];
     window.Store.SendClear = window.mR.findModule('sendClear')[0];
     window.Store.SendDelete = window.mR.findModule('sendDelete')[0];
@@ -150,7 +152,7 @@ exports.LoadUtils = () => {
             delete options.linkPreview;
             const link = window.Store.Validators.findLink(content);
             if (link) {
-                const preview = await window.Store.Wap.queryLinkPreview(link.url);
+                const preview = await window.Store.Query.queryLinkPreview(link.url);
                 preview.preview = true;
                 preview.subtype = 'url';
                 options = { ...options, ...preview };
