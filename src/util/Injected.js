75480a53e2773ff61ffbfeaccf5cd2b19e1e2f01
Rajeh Taher <rajeh@reforward.dev>
2022-01-26 01:08:03 +0200

    some really quick changes

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -5,7 +5,7 @@ exports.ExposeStore = (moduleRaidStr) => {
     eval('var moduleRaid = ' + moduleRaidStr);
     // eslint-disable-next-line no-undef
     window.mR = moduleRaid();
-    window.Store = Object.assign({}, window.mR.findModule('Chat')[0].default);
+    window.Store = Object.assign({}, window.mR.findModule('Chat').find(a => typeof a.default != 'undefined').default);
     window.Store.AppState = window.mR.findModule('STREAM')[0].Socket;
     window.Store.Conn = window.mR.findModule('Conn').find(a => typeof a.Conn != 'undefined').Conn;
     window.Store.BlockContact = window.mR.findModule('blockContact')[0];
