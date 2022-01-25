dff3ea069d37e54f686aac7c0ad446a0e1aba0d8
Rajeh Taher <rajeh@reforward.dev>
2022-01-26 01:11:41 +0200

    Update Injected.js

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -32,7 +32,7 @@ exports.ExposeStore = (moduleRaidStr) => {
     window.Store.SendDelete = window.mR.findModule('sendDelete')[0];
     window.Store.SendMessage = window.mR.findModule('addAndSendMsgToChat')[0];
     window.Store.SendSeen = window.mR.findModule('sendSeen')[0];
-    // window.Store.Sticker = window.mR.findModule('Sticker')[0].default.Sticker;
+    window.Store.Sticker = window.mR.findModule('Sticker')[0].default.Sticker;
     window.Store.User = window.mR.findModule('getMaybeMeUser')[0];
     window.Store.UploadUtils = window.mR.findModule((module) => (module.default && module.default.encryptAndUpload) ? module.default : null)[0].default;
     window.Store.USyncQuery = window.mR.findModule('USyncQuery')[0].USyncQuery;
