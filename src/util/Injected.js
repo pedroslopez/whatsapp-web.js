8e72e94742a92960f3ebe5e229368881c563394a
Pedro Lopez <pedroslopez@me.com>
2022-02-06 21:32:37 -0400

    fix: send messages properly when dissappearing mode is on

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -202,6 +202,12 @@ exports.LoadUtils = () => {
         const extraOptions = options.extraOptions || {};
         delete options.extraOptions;
 
+        const ephemeralSettings = {
+            ephemeralDuration: chat.isEphemeralSettingOn() ? chat.getEphemeralSetting() : undefined,
+            ephemeralSettingTimestamp: chat.getEphemeralSettingTimestamp() || undefined,
+            disappearingModeInitiator: chat.getDisappearingModeInitiator() || undefined,
+        };
+
         const message = {
             ...options,
             id: newMsgId,
@@ -214,6 +220,7 @@ exports.LoadUtils = () => {
             t: parseInt(new Date().getTime() / 1000),
             isNewMsg: true,
             type: 'chat',
+            ...ephemeralSettings,
             ...locationOptions,
             ...attOptions,
             ...quotedMsgOptions,
