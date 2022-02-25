b0d0940dd264197262f4a6f1175a3a68861c08ce
Pablo Paixão <zkyouta@hotmail.com>
2022-02-25 16:07:39 -0300

    Fix: Stream modify to Socket in APP State

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -6,7 +6,7 @@ exports.ExposeStore = (moduleRaidStr) => {
     // eslint-disable-next-line no-undef
     window.mR = moduleRaid();
     window.Store = Object.assign({}, window.mR.findModule(m => m.default && m.default.Chat)[0].default);
-    window.Store.AppState = window.mR.findModule('STREAM')[0].Socket;
+    window.Store.AppState = window.mR.findModule('Socket')[0].Socket;
     window.Store.Conn = window.mR.findModule('Conn')[0].Conn;
     window.Store.Wap = window.mR.findModule('queryLinkPreview')[0].default;
     window.Store.SendSeen = window.mR.findModule('sendSeen')[0];
