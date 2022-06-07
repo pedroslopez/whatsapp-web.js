7881353ebdf224c6d159f1e617389f9fa955e202
Shir Serlui <70711723+shirser121@users.noreply.github.com>
2022-06-07 07:41:40 +0300

    Fix models is undefined

--- src/Client.js
+++ src/Client.js
@@ -809,8 +809,8 @@ class Client extends EventEmitter {
                 return true;
             }
             const MAX_PIN_COUNT = 3;
-            if (window.Store.Chat.models.length > MAX_PIN_COUNT) {
-                let maxPinned = window.Store.Chat.models[MAX_PIN_COUNT - 1].pin;
+            if (window.Store.Chat._models.length > MAX_PIN_COUNT) {
+                let maxPinned = window.Store.Chat._models[MAX_PIN_COUNT - 1].pin;
                 if (maxPinned) {
                     return false;
                 }
@@ -1056,7 +1056,7 @@ class Client extends EventEmitter {
     async getChatsByLabelId(labelId) {
         const chatIds = await this.pupPage.evaluate(async (labelId) => {
             const label = window.Store.Label.get(labelId);
-            const labelItems = label.labelItemCollection.models;
+            const labelItems = label.labelItemCollection._models;
             return labelItems.reduce((result, item) => {
                 if (item.parentType === 'Chat') {
                     result.push(item.parentId);
@@ -1074,7 +1074,7 @@ class Client extends EventEmitter {
      */
     async getBlockedContacts() {
         const blockedContacts = await this.pupPage.evaluate(() => {
-            let chatIds = window.Store.Blocklist.models.map(a => a.id._serialized);
+            let chatIds = window.Store.Blocklist._models.map(a => a.id._serialized);
             return Promise.all(chatIds.map(id => window.WWebJS.getContact(id)));
         });
 
