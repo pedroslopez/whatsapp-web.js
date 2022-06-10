21a51d4bd042347a82f06ad74509d1ca8d573ec3
Pedro Lopez <pedroslopez@me.com>
2022-06-09 23:39:26 -0400

    fix: correctly loadEarlierMsgs with ConversationMsgs module

--- src/structures/Chat.js
+++ src/structures/Chat.js
@@ -183,7 +183,7 @@ class Chat extends Base {
 
             if (searchOptions && searchOptions.limit > 0) {
                 while (msgs.length < searchOptions.limit) {
-                    const loadedMessages = await chat.preload();
+                    const loadedMessages = await window.Store.ConversationMsgs.loadEarlierMsgs(chat);
                     if (!loadedMessages) break;
                     msgs = [...loadedMessages.filter(msgFilter), ...msgs];
                 }
