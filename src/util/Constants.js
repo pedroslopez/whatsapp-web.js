6e047cb9bee7eeb43943e1b900b4401cd14c7c78
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2022-08-09 18:26:45 +0300

    Update User agent (#1470)
    I encountered errors because of this (it says that the chrome version needs to be updated)
    
    Co-authored-by: Rajeh Taher <rajeh@reforward.dev>

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -11,7 +11,7 @@ exports.DefaultOptions = {
     qrMaxRetries: 0,
     takeoverOnConflict: false,
     takeoverTimeoutMs: 0,
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
     ffmpegPath: 'ffmpeg',
     bypassCSP: false
 };
