6e234df2ac8420645bbc7034ae31522f4fedb479
Your Name <allan.murara@gmail.com>
2022-08-18 17:04:54 -0300

    add Error event propagation at InterfaceController

--- src/util/InterfaceController.js
+++ src/util/InterfaceController.js
@@ -14,11 +14,16 @@ class InterfaceController {
      * @param {string} chatId ID of the chat window that will be opened
      */
     async openChatWindow(chatId) {
-        await this.pupPage.evaluate(async chatId => {
-            let chatWid = window.Store.WidFactory.createWid(chatId);
-            let chat = await window.Store.Chat.find(chatWid);
-            await window.Store.Cmd.openChatAt(chat);
-        }, chatId);
+        try {
+            await this.pupPage.evaluate(async chatId => {
+                let chatWid = window.Store.WidFactory.createWid(chatId);
+                let chat = await window.Store.Chat.find(chatWid);
+                await window.Store.Cmd.openChatAt(chat);
+            }, chatId);
+        } catch(err) {
+            throw err
+        }
+        
     }
 
     /**
@@ -26,10 +31,14 @@ class InterfaceController {
      * @param {string} chatId ID of the chat drawer that will be opened
      */
     async openChatDrawer(chatId) {
-        await this.pupPage.evaluate(async chatId => {
-            let chat = await window.Store.Chat.get(chatId);
-            await window.Store.Cmd.openDrawerMid(chat);
-        }, chatId);
+        try {
+            await this.pupPage.evaluate(async chatId => {
+                let chat = await window.Store.Chat.get(chatId);
+                await window.Store.Cmd.openDrawerMid(chat);
+            }, chatId);
+        } catch(err) {
+            throw err
+        }
     }
 
     /**
@@ -37,10 +46,14 @@ class InterfaceController {
      * @param {string} chatId ID of the chat search that will be opened
      */
     async openChatSearch(chatId) {
-        await this.pupPage.evaluate(async chatId => {
-            let chat = await window.Store.Chat.get(chatId);
-            await window.Store.Cmd.chatSearch(chat);
-        }, chatId);
+        try {
+            await this.pupPage.evaluate(async chatId => {
+                let chat = await window.Store.Chat.get(chatId);
+                await window.Store.Cmd.chatSearch(chat);
+            }, chatId);
+        } catch(err) {
+            throw err
+        }
     }
 
     /**
@@ -48,10 +61,18 @@ class InterfaceController {
      * @param {string} msgId ID of the message that will be scrolled to
      */
     async openChatWindowAt(msgId) {
-        await this.pupPage.evaluate(async msgId => {
-            let msg = await window.Store.Msg.get(msgId);
-            await window.Store.Cmd.openChatAt(msg.chat, msg.chat.getSearchContext(msg));
-        }, msgId);
+        try {
+            await this.pupPage.evaluate(async msgId => {
+                try {
+                    let msg = await window.Store.Msg.get(msgId);
+                    await window.Store.Cmd.openChatAt(msg.chat, msg.chat.getSearchContext(msg));
+                } catch(err) {
+                    throw err
+                }
+            }, msgId)
+        } catch(err) {
+            throw err            
+        }
     }
 
     /**
@@ -59,19 +80,31 @@ class InterfaceController {
      * @param {string} msgId ID of the message drawer that will be opened
      */
     async openMessageDrawer(msgId) {
-        await this.pupPage.evaluate(async msgId => {
-            let msg = await window.Store.Msg.get(msgId);
-            await window.Store.Cmd.msgInfoDrawer(msg);
-        }, msgId);
+        try {
+            await this.pupPage.evaluate(async msgId => {
+                try {
+                    let msg = await window.Store.Msg.get(msgId);
+                    await window.Store.Cmd.msgInfoDrawer(msg);
+                } catch(err) {
+                    throw err
+                }
+            }, msgId);
+        } catch(err) {
+            throw err
+        }
     }
 
     /**
      * Closes the Right Drawer
      */
     async closeRightDrawer() {
-        await this.pupPage.evaluate(async () => {
-            await window.Store.Cmd.closeDrawerRight();
-        });
+        try {
+            await this.pupPage.evaluate(async () => {
+                await window.Store.Cmd.closeDrawerRight();
+            });
+        } catch(err) {
+            throw err
+        }
     }
 
     /**
@@ -88,9 +121,17 @@ class InterfaceController {
      * @param {string} feature status to check
      */
     async checkFeatureStatus(feature) {
-        return await this.pupPage.evaluate((feature) => {
-            return window.Store.Features.supportsFeature(feature);
-        }, feature);
+        try {
+            return await this.pupPage.evaluate((feature) => {
+                try {
+                    return window.Store.Features.supportsFeature(feature);
+                } catch(err) {
+                    throw err
+                }
+            }, feature);
+        } catch(err) {
+            throw err
+        }
     }
 
     /**
@@ -100,7 +141,11 @@ class InterfaceController {
     async enableFeatures(features) {
         await this.pupPage.evaluate((features) => {
             for (const feature in features) {
-                window.Store.Features.setFeature(features[feature], true);
+                try {
+                    window.Store.Features.setFeature(features[feature], true);
+                } catch(err) {
+                    throw err
+                }
             }
         }, features);
     }
@@ -110,11 +155,15 @@ class InterfaceController {
      * @param {string[]} features to be disabled
      */
     async disableFeatures(features) {
-        await this.pupPage.evaluate((features) => {
-            for (const feature in features) {
-                window.Store.Features.setFeature(features[feature], false);
-            }
-        }, features);
+        try {
+            await this.pupPage.evaluate((features) => {
+                for (const feature in features) {
+                    window.Store.Features.setFeature(features[feature], false);
+                }
+            }, features);
+        } catch(err) {
+            throw err
+        }
     }
 }
 
