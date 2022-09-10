90513838bf002717c7c00251e4f9f7cd78c50a8a
blox <73058244+soudblox@users.noreply.github.com>
2022-09-10 11:51:21 +0700

    remove fromMe check

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -436,7 +436,7 @@ class Message extends Base {
         await this.client.pupPage.evaluate((msgId, everyone) => {
             let msg = window.Store.Msg.get(msgId);
 
-            if (everyone && msg.id.fromMe && msg._canRevoke()) {
+            if (everyone && msg._canRevoke()) {
                 return window.Store.Cmd.sendRevokeMsgs(msg.chat, [msg], {type: 'Sender'});
             }
 
