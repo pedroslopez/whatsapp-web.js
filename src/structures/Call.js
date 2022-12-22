b3440c8a4171399ab5147a58a43ac6384d661996
purpshell <rajeh@reforward.dev>
2022-12-22 17:53:45 +0200

    eslint is annoying

--- src/structures/Call.js
+++ src/structures/Call.js
@@ -69,7 +69,7 @@ class Call extends Base {
     async reject() {
         return this.client.pupPage.evaluate((peerJid, id) => {
             return window.WWebJS.rejectCall(peerJid, id);
-        }, this.from, this.id)
+        }, this.from, this.id);
     }
 }
 
