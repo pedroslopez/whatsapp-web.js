7dd8688f048faad658a6718a57f83488421735da
Rajeh Taher <rajeh@reforward.dev>
2023-01-15 20:32:28 +0200

    fix: Fix client-side message rendering issue when sending attachements through the library (#1905)

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -271,6 +271,7 @@ exports.LoadUtils = () => {
             ...ephemeralFields,
             ...locationOptions,
             ...attOptions,
+            ...(Object.keys(attOptions).length > 0 ? attOptions.toJSON() : {}),
             ...quotedMsgOptions,
             ...vcardOptions,
             ...buttonOptions,
