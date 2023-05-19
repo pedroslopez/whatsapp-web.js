081b455fa513758fb06de577973179ee1c82a04a
tofers <acerkot@mail.ru>
2023-05-19 19:25:54 +0300

    hotfix: newId  and deviceType (#2196)
    * update method generate Msg id, newId to newIdV2
    
    * replace newId and await in version WA 2.2321.6
    
    * fix deviceType

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -245,11 +245,12 @@ exports.LoadUtils = () => {
 
         const meUser = window.Store.User.getMaybeMeUser();
         const isMD = window.Store.MDBackend;
-
+        const newId = await window.Store.MsgKey.newId();
+        
         const newMsgId = new window.Store.MsgKey({
             from: meUser,
             to: chat.id,
-            id: window.Store.MsgKey.newId(),
+            id: newId,
             participant: isMD && chat.id.isGroup() ? meUser : undefined,
             selfDir: 'out',
         });
