765f64d15f5ff667ba40b34f068c5843e49fd67d
MasterMarques <41557647+MasterMarques@users.noreply.github.com>
2023-05-19 10:27:10 -0300

    Fix data.id.id.substring error
    Fix the error: 
    TypeError: data.id.id.substring is not a function

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -88,8 +88,9 @@ class Message extends Base {
         /**
          * String that represents from which device type the message was sent
          * @type {string}
+         * MasterMarques update
          */
-        this.deviceType = data.id.id.length > 21 ? 'android' : data.id.id.substring(0, 2) == '3A' ? 'ios' : 'web';
+        this.deviceType = typeof data.id.id === 'string' && data.id.id.length > 21 ? 'android' : typeof data.id.id === 'string' && data.id.id.substring(0, 2) === '3A' ? 'ios' : 'web';
 
         /**
          * Indicates if the message was forwarded
