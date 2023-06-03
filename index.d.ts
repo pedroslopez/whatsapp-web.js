0edd5de1c9e6eb3d3d48b62d74c8d023ede3c71c
LucianoHanna <lucianohanna@id.uff.br>
2023-06-03 17:37:16 -0300

    Update doc

--- index.d.ts
+++ index.d.ts
@@ -1119,6 +1119,7 @@ declare namespace WAWebJS {
          * @param {Number} [searchOptions.limit] The amount of messages to return. If no limit is specified, the available messages will be returned. Note that the actual number of returned messages may be smaller if there aren't enough messages in the conversation. Set this to Infinity to load all messages.
          * @param {Number} [searchOptions.minTimestamp] If specified, filter message by minimum timestamp.
          * @param {Number} [searchOptions.maxTimestamp] If specified, filter message by maximum timestamp.
+         * @param {Boolean} [searchOptions.fromMe] Return only messages from the bot number or vise versa. To get all messages, leave the option undefined.
          * @returns {Promise<Array<Message>>}
          */
         fetchMessages: (searchOptions: MessageSearchOptions) => Promise<Message[]>,
