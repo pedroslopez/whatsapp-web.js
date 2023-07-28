602cc5a48eb7f996f1ceb693b35992784ddb567b
antonio <antoniobvg@gmail.com>
2023-07-28 20:21:15 -0300

    Add support for sending large files (up to 1GB)
    Break the file down into 50MB chunks and send via batches, creating a string object for each chunk
    
    Afterwards decode each chunk into one bing string
    
    Todo:
    I am overriding the entire attachment into a new MessageMedia object. Overriding just the data field was not yielding results (it stayed the original big string)
    
    work is based out of:
    https://github.com/pedroslopez/whatsapp-web.js/compare/main...matricce:whatsapp-web.js:fix/sendBigFiles

--- src/Client.js
+++ src/Client.js
@@ -758,10 +758,6 @@ class Client extends EventEmitter {
      * @returns {Promise<Message>} Message that was just sent
      */
     async sendMessage(chatId, content, options = {}) {
-        if (options.mentions && options.mentions.some(possiblyContact => possiblyContact instanceof Contact)) {
-            console.warn('Mentions with an array of Contact are now deprecated. See more at https://github.com/pedroslopez/whatsapp-web.js/pull/2166.');
-            options.mentions = options.mentions.map(a => a.id._serialized);
-        }
         let internalOptions = {
             linkPreview: options.linkPreview === false ? undefined : true,
             sendAudioAsVoice: options.sendAudioAsVoice,
@@ -771,11 +767,13 @@ class Client extends EventEmitter {
             caption: options.caption,
             quotedMessageId: options.quotedMessageId,
             parseVCards: options.parseVCards === false ? false : true,
-            mentionedJidList: Array.isArray(options.mentions) ? options.mentions : [],
+            mentionedJidList: Array.isArray(options.mentions) ? options.mentions.map(contact => contact.id._serialized) : [],
             extraOptions: options.extra
         };
 
+
         const sendSeen = typeof options.sendSeen === 'undefined' ? true : options.sendSeen;
+
 
         if (content instanceof MessageMedia) {
             internalOptions.attachment = content;
@@ -801,7 +799,7 @@ class Client extends EventEmitter {
             internalOptions.list = content;
             content = '';
         }
-
+        
         if (internalOptions.sendMediaAsSticker && internalOptions.attachment) {
             internalOptions.attachment = await Util.formatToWebpSticker(
                 internalOptions.attachment, {
@@ -812,21 +810,67 @@ class Client extends EventEmitter {
             );
         }
 
+
+        const isBigFile = internalOptions.attachment?.data?.length > (1024 * 1024 * 79);
+
+        if (isBigFile) {
+			var startDivision = 2
+            var middle = internalOptions.attachment.data.length / startDivision;
+			while (middle > (1024 * 1024 * 50)){
+				startDivision += 1
+				middle = Math.floor(internalOptions.attachment.data.length / startDivision);
+			}
+			var currentIndex = 0;
+			var passes = 0;
+			while(currentIndex < internalOptions.attachment?.data?.length){
+				var chunkPiece = middle
+				if(currentIndex + middle > internalOptions.attachment?.data?.length){
+					chunkPiece = internalOptions.attachment?.data?.length - currentIndex
+				}
+				await this.pupPage.evaluate(async (chatId, chunk,passes) => {
+					if (chunk) {
+						window.Store[`mediaChunk_${chatId}_${passes}`] = chunk;
+					}
+				}, chatId, internalOptions.attachment.data.substring(currentIndex, currentIndex+chunkPiece),passes);
+				currentIndex += chunkPiece
+				passes += 1
+			}
+            internalOptions.attachment = new MessageMedia("document","dummy",internalOptions.attachment.filename);
+			await this.pupPage.evaluate(async (chatId,passes) => {
+					window.Store[`mediaChunk_${chatId}_passes`] = passes;	
+			}, chatId,passes);
+        }
+
         const newMessage = await this.pupPage.evaluate(async (chatId, message, options, sendSeen) => {
             const chatWid = window.Store.WidFactory.createWid(chatId);
             const chat = await window.Store.Chat.find(chatWid);
+
 
 
             if (sendSeen) {
                 window.WWebJS.sendSeen(chatId);
             }
 
+            if(window.Store[`mediaChunk_${chatId}_passes`]) {
+				options.attachment.data = ''
+                var maxPasses = window.Store[`mediaChunk_${chatId}_passes`]
+				var currPass = 0;
+				while (currPass < maxPasses){
+					options.attachment.data += window.Store[`mediaChunk_${chatId}_${currPass}`]
+					delete window.Store[`mediaChunk_${chatId}_${currPass}`];
+					currPass += 1;			
+				}
+				delete window.Store[`mediaChunk_${chatId}_passes`];
+            }
+
+
             const msg = await window.WWebJS.sendMessage(chat, message, options, sendSeen);
             return msg.serialize();
         }, chatId, content, internalOptions, sendSeen);
 
+
         return new Message(this, newMessage);
-    }
+    } 
     
     /**
      * Searches for messages
