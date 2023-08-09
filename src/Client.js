fb9343fdabbd912e92c696706d2461a18130e6ec
ALΞX <alechqos@gmail.com>
2023-08-09 18:30:09 +0300

    code refactoring

--- src/Client.js
+++ src/Client.js
@@ -275,14 +275,14 @@ class Client extends EventEmitter {
 
         }
 
-        /**
-         * Helper function that compares between two WWeb versions. Its purpose is to help the developer to choose the correct code implementation depending on the comparison value and the WWeb version.
-         * @param {string} lOperand The left operand for the WWeb version string to compare with
-         * @param {string} operator The comparison operator
-         * @param {string} rOperand The right operand for the WWeb version string to compare with
-         * @returns {boolean} Boolean value that indicates the result of the comparison
-         */
         await page.evaluate(() => {
+            /**
+             * Helper function that compares between two WWeb versions. Its purpose is to help the developer to choose the correct code implementation depending on the comparison value and the WWeb version.
+             * @param {string} lOperand The left operand for the WWeb version string to compare with
+             * @param {string} operator The comparison operator
+             * @param {string} rOperand The right operand for the WWeb version string to compare with
+             * @returns {boolean} Boolean value that indicates the result of the comparison
+             */
             window.compareWwebVersions = (lOperand, operator, rOperand) => {
                 if (!['>', '>=', '<', '<=', '='].includes(operator)) {
                     throw class _ extends Error {
@@ -318,7 +318,7 @@ class Client extends EventEmitter {
                 );
             };
         });
-        
+
         await page.evaluate(ExposeStore, moduleRaid.toString());
         const authEventPayload = await this.authStrategy.getAuthEventPayload();
 
