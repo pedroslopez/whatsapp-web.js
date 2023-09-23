e80274638e89ece18bd4c628af85e6e2dbd48812
alechkos <alechqos@gmail.com>
2023-09-23 05:07:13 +0300

    ChatFactory extended to create a Community

--- src/factories/ChatFactory.js
+++ src/factories/ChatFactory.js
@@ -2,11 +2,14 @@
 
 const PrivateChat = require('../structures/PrivateChat');
 const GroupChat = require('../structures/GroupChat');
+const Community = require('../structures/Community');
 
 class ChatFactory {
     static create(client, data) {
-        if(data.isGroup) {
-            return new GroupChat(client, data);
+        if (data.isGroup) {
+            return data.groupMetadata.isParentGroup
+                ? new Community(client, data)
+                : new GroupChat(client, data);
         }
 
         return new PrivateChat(client, data);
