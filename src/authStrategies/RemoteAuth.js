27c8123234d5c2f019b66ebe05ea68e484d6e98c
rithienatan <rprado@kryptontech.com.br>
2023-09-25 12:35:51 -0300

    Fix: RemoteAuth unzip .zip file properly.

--- src/authStrategies/RemoteAuth.js
+++ src/authStrategies/RemoteAuth.js
@@ -3,12 +3,12 @@
 /* Require Optional Dependencies */
 try {
     var fs = require('fs-extra');
-    var unzipper = require('unzipper');
     var archiver = require('archiver');
+    var AdmZip = require('adm-zip');
 } catch {
     fs = undefined;
-    unzipper = undefined;
     archiver = undefined;
+    AdmZip = undefined;
 }
 
 const path = require('path');
@@ -154,13 +154,14 @@ class RemoteAuth extends BaseAuthStrategy {
     }
 
     async unCompressSession(compressedSessionPath) {
-        var stream = fs.createReadStream(compressedSessionPath);
         await new Promise((resolve, reject) => {
-            stream.pipe(unzipper.Extract({
-                path: this.userDataDir
-            }))
-                .on('error', err => reject(err))
-                .on('finish', () => resolve());
+            const zip = new AdmZip(compressedSessionPath);
+            zip.extractAllToAsync(this.userDataDir, true, false, (err) => {
+                if(err)
+                { reject(err); }
+                else
+                { resolve(); }
+            });
         });
         await fs.promises.unlink(compressedSessionPath);
     }
