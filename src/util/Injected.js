fecf5fff072414b5551008467ce9be6c788d2491
alechkos <93551621+alechkos@users.noreply.github.com>
2024-01-11 03:27:22 +0200

    fix: fix links retrieving from message

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -501,10 +501,7 @@ exports.LoadUtils = () => {
 
         msg.isEphemeral = message.isEphemeral;
         msg.isStatusV3 = message.isStatusV3;
-        msg.links = (message.getRawLinks()).map(link => ({
-            link: link.href,
-            isSuspicious: Boolean(link.suspiciousCharacters && link.suspiciousCharacters.size)
-        }));
+        msg.links = (window.Store.Validators.findLinks(message.body)).map((link) => (link.href));
 
         if (msg.buttons) {
             msg.buttons = msg.buttons.serialize();
