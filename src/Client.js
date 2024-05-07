c3266b8acf924d8f229c25cad4654bd6f32f533f
Seow Zhen Jun <56989187+seowzhenjun0126@users.noreply.github.com>
2024-05-07 11:38:06 +0800

    Prevent breaking v2.2x

--- src/Client.js
+++ src/Client.js
@@ -1209,7 +1209,7 @@ class Client extends EventEmitter {
         const profilePic = await this.pupPage.evaluate(async contactId => {
             try {
                 const chatWid = window.Store.WidFactory.createWid(contactId);
-                return await window.Store.ProfilePic.requestProfilePicFromServer(chatWid);
+                return await (window.WWebJS.compareWwebVersions(window.Debug.VERSION, '<', '2.3000.0') ? window.Store.ProfilePic.profilePicFind(chatWid) : window.Store.ProfilePic.requestProfilePicFromServer(chatWid));
             } catch (err) {
                 if(err.name === 'ServerStatusCodeError') return undefined;
                 throw err;
