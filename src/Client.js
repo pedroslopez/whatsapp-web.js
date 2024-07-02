8034e8f5ad7411664eb22173fc68075d2820854e
tuyuribr <45042245+tuyuribr@users.noreply.github.com>
2024-07-02 13:32:46 -0300

    new group settings

--- src/Client.js
+++ src/Client.js
@@ -1400,9 +1400,15 @@ class Client extends EventEmitter {
             try {
                 createGroupResult = await window.Store.GroupUtils.createGroup(
                     {
-                        'title':title,
-                        'messageTimer':messageTimer,
-                        'parentGroupWid':parentGroupWid,
+                        'memberAddMode': options.memberAddMode === undefined ? true : options.memberAddMode,
+                        'membershipApprovalMode': options.membershipApprovalMode === undefined ? false : options.membershipApprovalMode,
+                        'announce': options.announce === undefined ? true : options.announce,
+                        'ephemeralDuration': messageTimer,
+                        'full': undefined,
+                        'parentGroupId': parentGroupWid,
+                        'restrict': options.restrict === undefined ? true : options.restrict,
+                        'thumb': undefined,
+                        'title': title,
                     },
                     participantWids
                 );
