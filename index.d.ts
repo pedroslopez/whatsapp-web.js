783db3bf73c7d04f7dcf2811504741a59001e940
rbkayz <62215539+rbkayz@users.noreply.github.com>
2024-07-03 17:10:24 +0530

    Updated index.d.ts with createGroupOptions

--- index.d.ts
+++ index.d.ts
@@ -599,6 +599,26 @@ declare namespace WAWebJS {
          * @default ''
          */
         comment?: string
+        /** If true, then members can add participants to group. If false then only admins can add members
+         * @default true
+         *
+         */
+        memberAddMode?: boolean,
+        /** If true, only admins can send messages to the group
+         * @default true
+         *
+         */
+        announce?: boolean,
+         /** If true, only admins can change info settings e.g. subject and description
+         * @default true
+         *
+         */
+        restrict?: boolean,
+         /** If true, members can request addition of members, which will go to approval to admins
+         * @default false
+         *
+         */
+        membershipApprovalMode?: boolean
     }
 
     /** An object that handles the result for createGroup method */
