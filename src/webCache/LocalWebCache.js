f56ddc0b1e930ac6b07d429cb6099815eecdb4a3
Robson Niedson <rniedson@gmail.com>
2024-07-20 09:40:46 -0300

    Fix version extraction in LocalWebCache.js to handle null match
    This pull request addresses an issue where the version extraction in LocalWebCache.js can throw an error if the regex match returns null. This fix ensures that the code safely handles null matches, preventing a TypeError.

--- src/webCache/LocalWebCache.js
+++ src/webCache/LocalWebCache.js
@@ -6,7 +6,7 @@ const { WebCache, VersionResolveError } = require('./WebCache');
 /**
  * LocalWebCache - Fetches a WhatsApp Web version from a local file store
  * @param {object} options - options
- * @param {string} options.path - Path to the directory where cached versions are saved, default is: "./.wwebjs_cache/" 
+ * @param {string} options.path - Path to the directory where cached versions are saved, default is: "./.wwebjs_cache/"
  * @param {boolean} options.strict - If true, will throw an error if the requested version can't be fetched. If false, will resolve to the latest version.
  */
 class LocalWebCache extends WebCache {
@@ -19,7 +19,7 @@ class LocalWebCache extends WebCache {
 
     async resolve(version) {
         const filePath = path.join(this.path, `${version}.html`);
-        
+
         try {
             return fs.readFileSync(filePath, 'utf-8');
         }
@@ -29,13 +29,28 @@ class LocalWebCache extends WebCache {
         }
     }
 
+    /**
+     * Save the HTML content and its version information.
+     * 
+     * @param {string} indexHtml - The HTML content to be saved.
+     */
     async persist(indexHtml) {
-        // extract version from index (e.g. manifest-2.2206.9.json -> 2.2206.9)
-        const version = indexHtml.match(/manifest-([\d\\.]+)\.json/)[1];
-        if(!version) return;
-   
+        // Use a regular expression to find the version in the manifest file name
+        const match = indexHtml.match(/manifest-([\d\\.]+)\.json/);
+
+        // If the version is found, extract it from the match result
+        const version = match ? match[1] : null;
+
+        // If no version is found, exit the function
+        if (!version) return;
+
+        // Create the file path for saving the HTML content
         const filePath = path.join(this.path, `${version}.html`);
+
+        // Ensure the directory exists, create it if necessary
         fs.mkdirSync(this.path, { recursive: true });
+
+        // Write the HTML content to the file
         fs.writeFileSync(filePath, indexHtml);
     }
 }
