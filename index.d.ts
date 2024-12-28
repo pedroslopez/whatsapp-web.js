2fcc5f7f102c580a2390ddf2f2e646dd461eadaf
Eonus <38079109+Eonus@users.noreply.github.com>
2024-12-28 17:13:35 +0100

    Add max retries for fs.promises.rm (#3351) (#113)
    Co-authored-by: devlikeapro <155617407+devlikepro@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -560,7 +560,8 @@ declare namespace WAWebJS {
         public dataPath?: string;
         constructor(options?: {
             clientId?: string,
-            dataPath?: string
+            dataPath?: string,
+            rmMaxRetries?: number
         })
     }
     
@@ -574,7 +575,8 @@ declare namespace WAWebJS {
             store: Store,
             clientId?: string,
             dataPath?: string,
-            backupSyncIntervalMs: number
+            backupSyncIntervalMs: number,
+            rmMaxRetries?: number
         })
     }
 
