66309ea9aa68948ec45c059b17a53e06b605e149
BenyFilho <168232825+BenyFilho@users.noreply.github.com>
2025-07-24 22:18:16 -0300

    Add method getContactLidOrPhone
    Co-authored-by: alechkos <93551621+alechkos@users.noreply.github.com>

--- src/Client.js
+++ src/Client.js
@@ -2189,7 +2189,9 @@ class Client extends EventEmitter {
     async getContactLidOrPhone(userId) {
         return await this.pupPage.evaluate(async (userId) => {
             const wId = await window.Store.WidFactory.createWid(userId);
-            return userId.indexOf('lid')>-1 ? await window.Store.LidUtils.getPhoneNumber(wId)._serialized : await window.Store.LidUtils.getCurrentLid(wId)._serialized;
+            return wid.server === 'lid'
+                ? window.Store.LidUtils.getPhoneNumber(wid)._serialized
+                : window.Store.LidUtils.getCurrentLid(wid)._serialized;
         }, userId);
     }
 }
