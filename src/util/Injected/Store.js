647fadfe7e78f2913aa0d035b57ac5821bb75bf2
alechkos <93551621+alechkos@users.noreply.github.com>
2025-09-11 22:30:38 +0300

    feat: add required module

--- src/util/Injected/Store.js
+++ src/util/Injected/Store.js
@@ -103,6 +103,7 @@ exports.ExposeStore = () => {
     window.Store.UploadUtils = window.require('WAWebUploadManager');
     window.Store.WAWebStreamModel = window.require('WAWebStreamModel');
     window.Store.FindOrCreateChat = window.require('WAWebFindChatAction');
+    window.Store.CustomerNoteUtils = window.require('WAWebNoteAction');
     
     window.Store.Settings = {
         ...window.require('WAWebUserPrefsGeneral'),
