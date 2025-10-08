6dbcf61547548a9020a78ed1028b43853974dd23
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2025-10-08 10:12:51 +0700

    update: whatsapp web version

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.3000.1017054665',
+    webVersion: '2.3000.1028123040',
     webVersionCache: {
         type: 'local',
     },
