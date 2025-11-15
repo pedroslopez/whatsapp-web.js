ea194bb634702fa988598add59145d95a52d9d24
pedroslopez <pedroslopez@users.noreply.github.com>
2025-11-15 21:20:20 +0000

    Update supported WhatsApp Web version to v2.3000.1029952067

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.3000.1017054665',
+    webVersion: '2.3000.1029952067',
     webVersionCache: {
         type: 'local',
     },
