a663953fd4d3c7003bf272dc6b6706d05ee8e968
Rajeh Taher <rajeh@reforward.dev>
2025-12-16 08:50:56 +0200

    general: tree-shaking pre-MD and old comet versions

--- tests/client.js
+++ tests/client.js
@@ -8,14 +8,12 @@ const Contact = require('../src/structures/Contact');
 const Message = require('../src/structures/Message');
 const MessageMedia = require('../src/structures/MessageMedia');
 const Location = require('../src/structures/Location');
-const LegacySessionAuth = require('../src/authStrategies/LegacySessionAuth');
-const { MessageTypes, WAState, DefaultOptions } = require('../src/util/Constants');
+const { MessageTypes, DefaultOptions } = require('../src/util/Constants');
 
 const expect = chai.expect;
 chai.use(chaiAsPromised);
 
 const remoteId = helper.remoteId;
-const isMD = helper.isMD();
 
 describe('Client', function() {
     describe('User Agent', function () {
@@ -139,139 +137,11 @@ describe('Client', function() {
             await client.initialize();
 
             expect(authenticatedCallback.called).to.equal(true);
-
-            if(helper.isUsingLegacySession()) {
-                const newSession = authenticatedCallback.args[0][0];
-                expect(newSession).to.have.key([
-                    'WABrowserId', 
-                    'WASecretBundle', 
-                    'WAToken1', 
-                    'WAToken2'
-                ]);
-            }
-            
             expect(readyCallback.called).to.equal(true);
             expect(qrCallback.called).to.equal(false);
 
             await client.destroy();
         });
-
-        describe('LegacySessionAuth', function () {
-            it('should fail auth if session is invalid', async function() {
-                this.timeout(40000);
-        
-                const authFailCallback = sinon.spy();
-                const qrCallback = sinon.spy();
-                const readyCallback = sinon.spy();
-        
-                const client = helper.createClient({
-                    options: {
-                        authStrategy: new LegacySessionAuth({
-                            session: {
-                                WABrowserId: 'invalid', 
-                                WASecretBundle: 'invalid', 
-                                WAToken1: 'invalid', 
-                                WAToken2: 'invalid'
-                            },
-                            restartOnAuthFail: false,
-                        }),
-                    }
-                });
-        
-                client.on('qr', qrCallback);
-                client.on('auth_failure', authFailCallback);
-                client.on('ready', readyCallback);
-        
-                client.initialize();
-        
-                await helper.sleep(25000);
-        
-                expect(authFailCallback.called).to.equal(true);
-                expect(authFailCallback.args[0][0]).to.equal('Unable to log in. Are the session details valid?');
-        
-                expect(readyCallback.called).to.equal(false);
-                expect(qrCallback.called).to.equal(false);
-        
-                await client.destroy();
-            });
-        
-            it('can restart without a session if session was invalid and restartOnAuthFail=true', async function() {
-                this.timeout(40000);
-        
-                const authFailCallback = sinon.spy();
-                const qrCallback = sinon.spy();
-        
-                const client = helper.createClient({
-                    options: {
-                        authStrategy: new LegacySessionAuth({
-                            session: {
-                                WABrowserId: 'invalid', 
-                                WASecretBundle: 'invalid', 
-                                WAToken1: 'invalid', 
-                                WAToken2: 'invalid'
-                            },
-                            restartOnAuthFail: true,
-                        }),
-                    }
-                });
-        
-                client.on('auth_failure', authFailCallback);
-                client.on('qr', qrCallback);
-        
-                client.initialize();
-        
-                await helper.sleep(35000);
-        
-                expect(authFailCallback.called).to.equal(true);
-                expect(qrCallback.called).to.equal(true);
-                expect(qrCallback.args[0][0]).to.have.length.greaterThanOrEqual(152);
-        
-                await client.destroy();
-            });
-        });
-
-        describe('Non-MD only', function () {
-            if(!isMD) {
-                it('can take over if client was logged in somewhere else with takeoverOnConflict=true', async function() {
-                    this.timeout(40000);
-    
-                    const readyCallback1 = sinon.spy();
-                    const readyCallback2 = sinon.spy();
-                    const disconnectedCallback1 = sinon.spy();
-                    const disconnectedCallback2 = sinon.spy();
-    
-                    const client1 = helper.createClient({
-                        authenticated: true, 
-                        options: { takeoverOnConflict: true, takeoverTimeoutMs: 5000 }
-                    });
-                    const client2 = helper.createClient({authenticated: true});
-    
-                    client1.on('ready', readyCallback1);
-                    client2.on('ready', readyCallback2);
-                    client1.on('disconnected', disconnectedCallback1);
-                    client2.on('disconnected', disconnectedCallback2);
-    
-                    await client1.initialize();
-                    expect(readyCallback1.called).to.equal(true);
-                    expect(readyCallback2.called).to.equal(false);
-                    expect(disconnectedCallback1.called).to.equal(false);
-                    expect(disconnectedCallback2.called).to.equal(false);
-    
-                    await client2.initialize();
-                    expect(readyCallback2.called).to.equal(true);
-                    expect(disconnectedCallback1.called).to.equal(false);
-                    expect(disconnectedCallback2.called).to.equal(false);
-    
-                    // wait for takeoverTimeoutMs to kick in
-                    await helper.sleep(5200);
-                    expect(disconnectedCallback1.called).to.equal(false);
-                    expect(disconnectedCallback2.called).to.equal(true);
-                    expect(disconnectedCallback2.calledWith(WAState.CONFLICT)).to.equal(true);
-    
-                    await client1.destroy();
-                });
-            }
-        }); 
     });
 
     describe('Authenticated', function() {
