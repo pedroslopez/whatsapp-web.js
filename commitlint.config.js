6525ad1632dce6a08635fbfb332bd412f827baf4
2hoch1 <mail@2hoch1.dev>
2026-02-21 16:56:42 +0100

    infra(commitlint): update config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 92],
+    'header-max-length': [2, 'always', 72],
     'type-enum': [
       2,
       'always',
