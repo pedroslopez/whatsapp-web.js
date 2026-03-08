c2d978fd0af74db4addbf3bb62475b1124512f96
BenyFilho <168232825+BenyFilho@users.noreply.github.com>
2026-03-08 11:56:04 -0300

    Fix Block Contact (#127093)

--- src/structures/Contact.js
+++ src/structures/Contact.js
@@ -155,8 +155,8 @@ class Contact extends Base {
         if(this.isGroup) return false;
 
         await this.client.pupPage.evaluate(async (contactId) => {
-            const contact = (window.require('WAWebCollections')).Contact.get(contactId);
-            await (window.require('WAWebBlockContactAction')).blockContact({contact});
+            const chat = await window.WWebJS.getChat(contactId);
+            await (window.require('WAWebBlockContactAction')).blockContact({'contact':chat});
         }, this.id._serialized);
 
         this.isBlocked = true;
