d8209738c832d6ee7af8fe8f045bb1151872d16d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2026-03-14 20:12:07 +0700

    update user agent data

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -16,7 +16,7 @@ exports.DefaultOptions = {
     takeoverOnConflict: false,
     takeoverTimeoutMs: 0,
     userAgent:
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
     ffmpegPath: 'ffmpeg',
     bypassCSP: false,
     proxyAuthentication: undefined,
