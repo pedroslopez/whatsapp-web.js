5e37981795ab12a0507f1d7cf9f01a41ef0e01e6
BenyFilho <168232825+BenyFilho@users.noreply.github.com>
2026-03-15 12:04:35 +0000

    chore: update supported WhatsApp Web version to v2.3000.1035204344

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null,
     },
-    webVersion: '2.3000.1017054665',
+    webVersion: '2.3000.1035204344',
     webVersionCache: {
         type: 'local',
     },
